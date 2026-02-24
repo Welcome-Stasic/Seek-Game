8b30f057c19fa0b9ad2eda4a1d0073dff2aa56c7
Стас <TimeToAgain@tutamail.com>
2026-02-24 21:42:37 +0500

    пути

--- js/sounds.js
+++ js/sounds.js
@@ -1,20 +1,20 @@
-const bgMusic = new Audio("/sounds/background.mp3");
+const bgMusic = new Audio("sounds/background.mp3");
 bgMusic.loop = true;
 bgMusic.volume = 0.3;
 
 export const sound = {
   playFound: () => {
-    const audio = new Audio("/sounds/found.mp3");
+    const audio = new Audio("sounds/found.mp3");
     audio.volume = 0.4;
     audio.play();
   },
   playWrong: () => {
-    const audio = new Audio("/sounds/wrong.mp3");
+    const audio = new Audio("sounds/wrong.mp3");
     audio.volume = 0.3;
     audio.play();
   },
   playVictory: () => {
-    const audio = new Audio("/sounds/victory.mp3");
+    const audio = new Audio("sounds/victory.mp3");
     audio.volume = 0.5;
     audio.play();
   },
